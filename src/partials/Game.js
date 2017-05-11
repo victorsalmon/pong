cdd1bdf6864576312733e228376bf7d2831ce8c2
Victor Salmon <vasalmon@hotmail.com>
2017-05-11 13:57:54 -0700

    Generate SVG dynamically in game loop

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,3 +1,4 @@
+import { SVG_NS } from '../settings';
 export default class Game {
 
 	constructor(element, width, height) {
@@ -5,11 +6,15 @@ export default class Game {
 		this.height = height;
 	
 		this.gameElement = document.getElementById (element);
-		// Other code goes here...
+
 	}
 
 	render() {
-		// More code goes here...
+		const svg = document.createElementNS(SVG_NS, 'svg');
+		svg.setAttributeNS(null, 'width', this.width);
+		svg.setAttributeNS(null, 'height', this.height);
+		svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
+		this.gameElement.innerHTML(svg);
 	}
 
 }
