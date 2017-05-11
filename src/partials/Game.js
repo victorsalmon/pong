83d37ab08ee60dd911c1444662f7b10ce78de6f7
Victor Salmon <vasalmon@hotmail.com>
2017-05-11 13:57:20 -0700

    Comment out pong board, define namespace on game.jjs

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,10 +1,10 @@
 export default class Game {
 
 	constructor(element, width, height) {
-		this.element = element;
 		this.width = width;
 		this.height = height;
 	
+		this.gameElement = document.getElementById (element);
 		// Other code goes here...
 	}
 
